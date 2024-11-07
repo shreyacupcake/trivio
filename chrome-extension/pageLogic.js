ff9c187982d8a046d9d59672da78b3056fbfc08e
premJyotish007 <bishnoi4@illinois.edu>
2024-11-06 23:09:52 -0600

    chrome extension setup

---
+++ chrome-extension/pageLogic.js
@@ -0,0 +1,12 @@
+
+// const currentUrl = window.location.href;
+// console.log(currentUrl);
+// pass currentUrl to flask app and get questions
+
+
+// Check if the play button exists and then click it
+if (document.querySelector('.ytp-play-button')) {
+    document.querySelector('.ytp-play-button').click();
+} else {
+  console.log("Play button not found.");
+}
--- chrome-extension/sidepanel.js
+++ chrome-extension/sidepanel.js
@@ -1,1 +1,19 @@
-console.log('sidepanel.js');
+
+document.getElementById("listClasses").addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        files: ["pageLogic.js"],
+      });
+    });
+  });
+
+
+  document.getElementById("playButton").addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        files: ["pageLogic.js"],
+      });
+    });
+  });
